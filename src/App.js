ed0acac8d147be91f04bc6bfb6680c48c375a7b8
jaybon1 <ponggas2@gmail.com>
2022-11-12 16:18:02 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { Card, ListGroup, Spinner } from "react-bootstrap";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [postId, setPostId] = useState(1);
+
+  const [post, setPost] = useState(null);
+
+  const getPost = useCallback(() => {
+    setIsLoading(true);
+    axios
+      .get(
+        `https://045f2392-08f7-43cf-a537-6b008a6c346b.mock.pstmn.io/posts/${postId}`
+      )
+      .then((response) => {
+        // console.log(response);
+        if (response.status === 200) {
+          setPost(response.data);
+        } else {
+          alert("잘못된 데이터 입니다.");
+        }
+      })
+      .catch((error) => {
+        // console.log(error);
+        if (error.response.status === 404) {
+          alert("페이지가 없습니다.");
+        } else {
+          alert("문제가 발생하였습니다. 개발자에게 연락주세요.");
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+        console.log("무조건 실행됨");
+      });
+  }, [postId]);
+
+  useEffect(() => {
+    getPost();
+  }, [postId]);
+
+  return (
+    <div>
+      {isLoading || post == null ? (
+        <div>로딩 중...</div>
+      ) : (
+        <Card style={{ width: "500px" }}>
+          <Card.Header>Post 1 데이터</Card.Header>
+          <ListGroup variant="flush">
+            <ListGroup.Item>{post.userId}</ListGroup.Item>
+            <ListGroup.Item>{post.id}</ListGroup.Item>
+            <ListGroup.Item>{post.title}</ListGroup.Item>
+            <ListGroup.Item>{post.body}</ListGroup.Item>
+            <ListGroup.Item>
+              <div>댓글 ({post.comments.length})</div>
+              {post.comments.map((item, index) => {
+                return (
+                  <Card style={{ width: "100%" }} key={index}>
+                    <Card.Header>{item.email}</Card.Header>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>{item.body}</ListGroup.Item>
+                    </ListGroup>
+                  </Card>
+                );
+              })}
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      )}
+      <button
+        onClick={() => setPostId((prev) => prev + 1)}
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <div>
+            <Spinner animation="border" size="sm" />
+            로딩 중...
+          </div>
+        ) : (
+          "다음글 보기"
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default App;
